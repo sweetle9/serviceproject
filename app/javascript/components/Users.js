74b4b60e06326bc75a196206b8b226fa71f0857a
sweetle9 <kaltauskaite.ingrida@gmail.com>
2022-03-14 19:06:51 +0300

    initial commit

---
+++ app/javascript/components/Users.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Users extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>All Users</h1>
+        <ul>
+          {this.props.users.map(user => (
+            <li key={user.id}>{`${user.id} ${user.email} ${user.admin} ${user.username}`}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+Users.propTypes = {
+  users: PropTypes.array
+};
+export default Users
